f6277974cb2630e3d9704eec0bfcf7f96fc7ac20
fauzanzaid <fauzanzaid.dev@gmail.com>
2018-02-04 11:15:45 +0530

    Implement Token new and destroy functions

---
+++ src/Token.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+#include "Token.h"
+
+
+////////////////////////////////
+// Constructors & Destructors //
+////////////////////////////////
+
+Token *Token_new(){
+	Token *tkn_ptr = malloc( sizeof(Token) );
+	tkn->data = Token_Data_new();
+}
+
+void Token_destroy(Token *tkn_ptr){
+	Token_Data_destroy(tkn_ptr->data);
+	free(tkn_ptr);
+}
